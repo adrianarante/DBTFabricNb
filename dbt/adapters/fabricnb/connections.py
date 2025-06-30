1d5ebd610929f1414fd7be55d8ccfe93786b2897
jbuhayan <josiah.buhayan@insight.com>
2025-06-30 13:10:54 +0800

    Initial branch push. - Just added some classes from the original dbt with spark, added the livy. - and methods to handle network overrides and Livy session management. Commented out unreachable statements.

--- dbt/adapters/fabricnb/connections.py
+++ dbt/adapters/fabricnb/connections.py
@@ -7,6 +7,16 @@ from dbt.adapters.sql import SQLConnectionManager as connection_cls
 
 from dbt.logger import GLOBAL_LOGGER as logger
 
+from typing import Any, Optional, Union, Tuple, List, Generator, Iterable, Sequence
+from abc import ABC, abstractmethod
+from dbt.dataclass_schema import StrEnum
+from dbt.contracts.connection import ConnectionState, AdapterResponse
+from dbt.events import AdapterLogger
+from livysession import LivySessionManager, LivySessionConnectionWrapper
+import time
+import json 
+import re
+
 @dataclass
 class FabricNbCredentials(Credentials):
     """
@@ -44,6 +54,45 @@ class FabricNbCredentials(Credentials):
         List of keys to display in the `dbt debug` output.
         """
         return ("host","port","username","user")
+    
+class FabricConnectionMethod(StrEnum):
+    # TODO probably should change this
+    LIVY = "livy"
+
+class FabricConnectionWrapper(ABC):
+    @abstractmethod
+    def cursor(self) -> "FabricConnectionWrapper":
+        pass
+
+    @abstractmethod
+    def cancel(self) -> None:
+        pass
+
+    @abstractmethod
+    def close(self) -> None:
+        pass
+
+    @abstractmethod
+    def rollback(self) -> None:
+        pass
+
+    @abstractmethod
+    def fetchall(self) -> Optional[List]:
+        pass
+
+    @abstractmethod
+    def execute(self, sql: str, bindings: Optional[List[Any]] = None) -> None:
+        pass
+
+    @property
+    @abstractmethod
+    def description(
+        self,
+    ) -> Sequence[
+        Tuple[str, Any, Optional[int], Optional[int], Optional[int], Optional[int], bool]
+    ]:
+        pass
+
 
 class FabricNbConnectionManager(connection_cls):
     TYPE = "fabricnb"
@@ -71,7 +120,7 @@ class FabricNbConnectionManager(connection_cls):
         pass
 
     @classmethod
-    def open(cls, connection):
+    def open(cls, connection: Connection) -> Connection:
         """
         Receives a connection object and a Credentials object
         and moves it to the "open" state.
@@ -94,7 +143,66 @@ class FabricNbConnectionManager(connection_cls):
         #     connection.state = "open"
         #     connection.handle = handle
         # return connection
-        pass
+        # pass
+        """Need to override the SparkConnectionManager class to use fabric-sparknb instead of fabric-spark"""
+        if connection.state == ConnectionState.OPEN:
+            logger.debug("Connection is already open, skipping open.")
+            return connection
+
+        creds = connection.credentials
+        exc = None
+        handle: FabricConnectionWrapper = None
+
+        for i in range(1 + creds.connect_retries):
+            try:
+                if creds.method == FabricConnectionMethod.LIVY:
+                    try:
+                        thread_id = cls.get_thread_identifier()
+                        if thread_id not in cls.connection_managers:
+                            cls.connection_managers[thread_id] = LivySessionManager()
+                        handle = LivySessionConnectionWrapper(
+                            cls.connection_managers[thread_id].connect(creds)
+                        )
+                        connection.state = ConnectionState.OPEN
+                        # SparkConnectionManager.fetch_spark_version(handle)
+                    except Exception as ex:
+                        logger.debug("Connection error: {}".format(ex))
+                        connection.state = ConnectionState.FAIL
+                else:
+                    raise dbt.exceptions.DbtProfileError(
+                        f"invalid credential method: {creds.method}"
+                    )
+                break
+            except Exception as e:
+                exc = e
+                if isinstance(e, EOFError):
+                    # The user almost certainly has invalid credentials.
+                    # Perhaps a token expired, or something
+                    msg = "Failed to connect"
+                    if creds.token is not None:
+                        msg += ", is your token valid?"
+                    raise dbt.exceptions.FailedToConnectError(msg) from e
+                retryable_message = _is_retryable_error(e)
+                if retryable_message and creds.connect_retries > 0:
+                    msg = (
+                        f"Warning: {retryable_message}\n\tRetrying in "
+                        f"{creds.connect_timeout} seconds "
+                        f"({i} of {creds.connect_retries})"
+                    )
+                    logger.warning(msg)
+                    time.sleep(creds.connect_timeout)
+                elif creds.retry_all and creds.connect_retries > 0:
+                    msg = cls._build_retry_all_message(exc, creds, i)
+                    logger.warning(msg)
+                    time.sleep(creds.connect_timeout)
+                else:
+                    raise dbt.exceptions.FailedToConnectError("failed to connect") from e
+        else:
+            raise exc  # type: ignore
+
+        connection.handle = handle
+        connection.state = ConnectionState.OPEN
+        return connection
 
     @classmethod
     def get_response(cls,cursor):
